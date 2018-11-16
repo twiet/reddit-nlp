a3855564f72a296c077b6b9234f380cb4a6d065a
Kevin Li <kevinli@Kevins-MacBook-Pro.local>
2018-11-15 20:42:15 -0800

    clean up

--- parser.py
+++ parser.py
@@ -32,28 +32,34 @@ supercategories = json.loads('''
   ]
 }
 ''')
+  
+def praw_call():
+  # reddit api object instance
+  # docs https://praw.readthedocs.io/en/latest/getting_started/quick_start.html
 
-# reddit api object instance
-# docs https://praw.readthedocs.io/en/latest/getting_started/quick_start.html
+  reddit = praw.Reddit(client_id='6Ac8q3UdUrJR2A',
+                      client_secret='unN_bfTDr5Gd-BibYqoMs90GxPQ',
+                      user_agent='parser.py')
 
-reddit = praw.Reddit(client_id='6Ac8q3UdUrJR2A',
-                     client_secret='unN_bfTDr5Gd-BibYqoMs90GxPQ',
-                     user_agent='parser.py')
+  l = []
+  for label, subreddits in supercategories.items():
+    for subreddit in subreddits:
+      # search and retrieve subreddit object via string
+      sub = reddit.subreddit(subreddit)
+      # find top post for subreddit
+      top_post = list(sub.top(limit=10))
+      for post in top_post:
+        # get top ten comments for post
+        comments = [comment.body for comment in post.comments[:10]]
+        # build and append data object
+        data = { "data" : { "title": post.title, "comments": comments }, "label": label }
+        l.append(data)
+  obj = { "corpus": l }
+  # dump results formatted to json
+  with open('reddit-nlp/samples.json', 'w') as outfile:
+    json.dump(obj, outfile, sort_keys=True, indent=4, separators=(',', ': '))
 
-l = []
-for label, subreddits in supercategories.items():
-  for subreddit in subreddits:
-    # search and retrieve subreddit object via string
-    sub = reddit.subreddit(subreddit)
-    # find top post for subreddit
-    top_post = list(sub.top(limit=1))[0]
-    # get top ten comments for post
-    comments = [comment.body for comment in top_post.comments[:10]]
-    # build and append data object
-    data = { "data" : { "title": top_post.title, "comments": comments }, "label": label }
-    l.append(data)
-obj = { "corpus": l }
-# dump results formatted to json
-s = json.dumps(obj, sort_keys=True, indent=4, separators=(',', ': '))
+def request():
+  print
 
-print(s)
+praw_call()

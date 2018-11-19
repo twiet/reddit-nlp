88c74e2696f48290cd8a4205f7d409b6a6b697aa
MZhang <zhmwkaka@gmail.com>
2018-11-19 12:35:52 -0800

    handle encode error

--- clean.py
+++ clean.py
@@ -11,7 +11,9 @@ def main():
     punc = re.compile(r"[\(\)\[\]\{\}`~/\\<>\*\-\+\&\^%$#@_=\|]")
     white = re.compile(r"\s+")
     url = re.compile(r"\S*://\S+")
+    quote = re.compile(r"\u2018|\u2019")
     dataf = open(sys.argv[1], "r")
+    logf = open("log.txt", "w")
     for line in dataf:
         data = json.loads(line)
         label = data["label"]
@@ -19,13 +21,26 @@ def main():
         content = deleted.sub(" ", content)
         content = url.sub(" ", content)
         content = html.unescape(content)
+        content = quote.sub("\'", content)
         content = punc.sub(" ", content)
         content = white.sub(" ", content)
         tokens = content.split(" ")
         if len(tokens) > 300:
             content = " ".join(tokens[0:300])
-        labelf.write(label + "\n")
-        contentf.write(content + "\n")
+        while True:
+            try:
+                contentf.write(content + "\n")
+            except UnicodeEncodeError as err:
+                content = content[:err.start] + content[err.end:]
+                continue
+            break
+        while True:
+            try:
+                labelf.write(label + "\n")
+            except UnicodeEncodeError as err:
+                label = label[:err.start] + label[err.end:]
+                continue
+            break
     labelf.close()
     contentf.close()
     dataf.close()
